74d9ffdb5a226e66754261bad512fafe6e43962e
Matthew Martin <mmartin724@yahoo.com>
2013-10-31 22:50:42 -0400

    camera commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+	
+	private float minY = -80f;
+	private float maxY = 80f;
+	
+	private float sensitivity = 10f;
+	
+	private float rotX;
+	private float rotY;
+	
+	void LateUpdate(){
+		
+		rotX += Input.GetAxis("Mouse X") * sensitivity;
+		
+		rotY += Input.GetAxis("Mouse Y") * sensitivity;
+		rotY = Mathf.Clamp(rotY, minY, maxY);
+		
+		transform.rotation = Quaternion.Euler(-rotY, rotX, 0);
+		
+	}
+}
